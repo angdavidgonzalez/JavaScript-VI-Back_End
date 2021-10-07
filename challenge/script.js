1e4d4a98eccd9c07d878b97c12465257e8e07d68
angdavidgonzalez <83287887+angdavidgonzalez@users.noreply.github.com>
2021-10-07 18:01:23 -0500

    Add files via upload

---
+++ challenge/script.js
@@ -0,0 +1,26 @@
+var RedesSociales = {
+  facebook : 'http://facebook.com/viewsource',
+  twitter: 'http://twitter.com/planetoftheweb',
+  flickr: 'http://flickr.com/planetotheweb',
+  youtube: 'http://youtube.com/planetoftheweb',
+  wordpress: 'https://wordpress.com/create',
+  linkedin: 'http://linkedin.com/create',
+  google: 'http://https://www.google.com/'
+
+};
+
+
+function mostrarRedes( RedesSociales ){
+  var nodoMostrarIconos = document.querySelectorAll('.socialmediaicons');
+  nodoMostrarIconos.innerHTML = "";
+
+  for (var i = 0; i < RedesSociales.length; i++) {
+    contenidoAMostrar = contenidoAMostrar + '<div id = "ciudades' + i + '">';
+    contenidoAMostrar += `<a href="' + RedesSociales[id] + '">' +
+        '<img src="images/' + id + '.png" alt="icon for '+id+'">' +
+        '</a>`;
+    nodoMostrarIconos.innerHTML ="contenidoAMostrar";
+  }
+}
+
+
